6dd58d0b2780022d6e05cfbc98de720df21dd956
Safta Catalin Mihai <saftacatalinmihai@gmail.com>
2015-01-27 23:01:26 +0200

    added url open target strategies: new tab/iframe/replace

--- request_server.pl
+++ request_server.pl
@@ -27,12 +27,12 @@ use Data::Printer;
 
 my $websocket_listeners = {};
 
-sub send_listeners{
-	my ($channel, $msg) =  @_;
+sub send_json_listeners{
+	my ($channel, $hash) =  @_;
 
 	if (exists $websocket_listeners->{$channel}) {
 		for my $l (keys %{$websocket_listeners->{$channel}} )  {
-			$websocket_listeners->{$channel}->{$l}->send($msg);
+			$websocket_listeners->{$channel}->{$l}->send({json =>$hash});
 		}
 	}
 }
@@ -54,7 +54,6 @@ websocket '/listener/register' => sub {
 };
 
 websocket '/listener/register/*channel' => sub {
-# websocket '/listener/register' => sub {
 	my $c = shift;
 	my $channel = $c->param("channel");
 
@@ -67,11 +66,11 @@ websocket '/listener/register/*channel' => sub {
 	my $id = "$c->tx";
 	$websocket_listeners->{$channel}->{$id} = $c->tx;
 
-	$c->on(message => sub {
-			my ($c, $message) = @_;
-			$c->app->log->debug("Recieved: $message");
-			$c->send("pong") if $message eq 'ping';
-		});
+	$c->on(json => sub {
+		my ($c, $hash) = @_;
+		$c->app->log->debug("Recieved: $hash->{msg}");
+		$c->send({json => {msg => "pong"}}) if $hash->{msg} eq 'ping';
+	});
 
 	$c->on(finish => sub {
 			$c->app->log->debug("Client disconected from channel $channel");
@@ -84,7 +83,11 @@ get '/open' => sub {
 		my $ch = $c->param('ch') ? $c->param('ch') : "public";
 		my $url_param = defined $c->param('url') ? $c->param('url') :  ${\$c->param()};
 
-		send_listeners($ch, $url_param);
+		my $page_url_target = $c->param("target") ?  $c->param("target") : "new_tab";
+		$page_url_target = 'new_tab' if ( !($page_url_target  =~ /^(new_tab|iframe|replace)$/));
+
+		$c->app->log->debug("Open request for url: [$url_param], taget: [$page_url_target]");
+		send_json_listeners($ch, {url => $url_param, page_url_target => $page_url_target});
 		$c->render(text => $url_param);
 };
 
@@ -95,27 +98,62 @@ __DATA__
 @@ index.html.ep
 <!DOCTYPE html>
 <html>
-	<head><title>Web-Share</title></head>
+	<head>
+		<title>Web-Share</title>
+		<style>
+			body {
+				position: relative;
+				margin: 0;
+				height: 100%;
+				padding-top: 100px;
+			}
+			iframe {
+				width: 100%;
+				height: 100%;
+				border: none;
+			}
+			.info {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100px;
+			}
+		</style>
+	</head>
 	<body>
 		<p> Channel:  <%= $ch %></p>
+		<div class='info'>
+
+		</div>
+		<iframe src="http://www.w3schools.com"></iframe>
 		<script>
 		var ws = new WebSocket('<%= url_for('listenerregister')->to_abs %>/<%= $ch %>');
 		// Incoming messages
 		ws.onmessage = function(event) {
-			var data = event.data;
-			if (data !== 'pong') {
-				console.log(data);
-				document.body.innerHTML += data + '<br/>';
-				var href = "http://"+data
+			var url = JSON.parse(event.data).url;
+			var page_url_target = JSON.parse(event.data).page_url_target;
+			if (url !== 'pong') {
+				console.log(url);
+				console.log(page_url_target);
+				document.body.innerHTML += url + '<br/>';
+				var href = "http://"+url
 
-				window.location.href = href;
-				// window.open("http://"+data,  "_blank");
+				switch (page_url_target) {
+					case "new_tab":
+						window.open(href,  "_blank");
+					case "replace":
+						window.location.href = href;
+					case "iframe":
+						document.getElementsByTagName('iframe')[0].src = href;
+				}
+
 			} else {
 				console.log('pong recieved');
 			}
 		};
-		window.setInterval(function () { ws.send('ping') }, 50000);
-		// window.setInterval(function () { ws.send('ping') }, 3000);
+		window.setInterval(function () { ws.send(JSON.stringify({msg: 'ping'})) }, 50000);
+		// window.setInterval(function () { ws.send(JSON.stringify({msg: 'ping'}))}, 3000);
 		</script>
 		<p>Listener registered, awaiting requests...</p>
 	</body>
