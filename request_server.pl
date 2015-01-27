658d917a38a07e8b28d0b8f3739ed6469e9c6789
Safta Catalin Mihai <saftacatalinmihai@gmail.com>
2015-01-27 21:13:32 +0200

    added channel support but maintained backwards compatibility

--- request_server.pl
+++ request_server.pl
@@ -23,29 +23,49 @@ use strict;
 use warnings;
 use utf8;
 use Mojolicious::Lite;
+use Data::Printer;
 
 my $websocket_listeners = {};
 
 sub send_listeners{
-	my $msg = shift;
-	for my $l (keys %$websocket_listeners) {
-		$websocket_listeners->{$l}->send($msg);
+	my ($channel, $msg) =  @_;
+
+	if (exists $websocket_listeners->{$channel}) {
+		for my $l (keys %{$websocket_listeners->{$channel}} )  {
+			$websocket_listeners->{$channel}->{$l}->send($msg);
+		}
 	}
 }
 
-get '/' => 'index';
+get '/' => sub {
+	my $c = shift;
+	$c->stash(ch => 'public');
+	$c->render(template => 'index', format => 'html');
+};
+
+get '/channel/*ch' => sub {
+		my $c = shift;
+		$c->render(template => 'index', format => 'html' );
+};
 
 websocket '/listener/register' => sub {
+	my $c= shift;
+	return $c->redirect_to("/listener/register/public");
+};
+
+websocket '/listener/register/*channel' => sub {
+# websocket '/listener/register' => sub {
 	my $c = shift;
+	my $channel = $c->param("channel");
 
 	# Remove inactivity timeout
 	$c->inactivity_timeout(0);
 
   	# Opened
-  	$c->app->log->debug('Client connected');
+  	$c->app->log->debug("Client connected on channel $channel");
 
 	my $id = "$c->tx";
-	$websocket_listeners->{$id} = $c->tx;
+	$websocket_listeners->{$channel}->{$id} = $c->tx;
 
 	$c->on(message => sub {
 			my ($c, $message) = @_;
@@ -54,15 +74,18 @@ websocket '/listener/register' => sub {
 		});
 
 	$c->on(finish => sub {
-			$c->app->log->debug("Client disconected");
-			delete $websocket_listeners->{$id};
+			$c->app->log->debug("Client disconected from channel $channel");
+			delete $websocket_listeners->{$channel}->{$id};
 		});
 };
 
 get '/open' => sub {
 		my $c = shift;
-		send_listeners($c->param);
-		$c->render(text => $c->param);
+		my $ch = $c->param('ch') ? $c->param('ch') : "public";
+		my $url_param = defined $c->param('url') ? $c->param('url') :  ${\$c->param()};
+
+		send_listeners($ch, $url_param);
+		$c->render(text => $url_param);
 };
 
 app->start;
@@ -74,15 +97,16 @@ __DATA__
 <html>
 	<head><title>Web-Share</title></head>
 	<body>
+		<p> Channel:  <%= $ch %></p>
 		<script>
-		var ws = new WebSocket('<%= url_for('listenerregister')->to_abs %>');
+		var ws = new WebSocket('<%= url_for('listenerregister')->to_abs %>/<%= $ch %>');
 		// Incoming messages
 		ws.onmessage = function(event) {
 			var data = event.data;
 			if (data !== 'pong') {
 				console.log(data);
 				document.body.innerHTML += data + '<br/>';
-				var href = "http://"+data 
+				var href = "http://"+data
 
 				window.location.href = href;
 				// window.open("http://"+data,  "_blank");
